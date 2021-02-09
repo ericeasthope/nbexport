ec6b6c21b74b9ae3bc9ffad1935e10f71fde2c5c
Eric Easthope <eric.easthope@me.com>
2021-02-08 16:22:04 -0800

    Move incomplete files to non-versioned directory

--- nb2md/nb2md.py
+++ nb2md/nb2md.py
@@ -1,7 +1,33 @@
-from nbparse.parser import Parser
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Authored by Eric Easthope
+"""
+
+from parser import Parser
+
+# Initialize notebook interpreter
 parser = Parser()
+
+# Get notebooks in directory
 parser.get_notebooks()
+
+# Markdown and LaTeX post-processing
+# Add JavaScipt tags
+# Replace images with Liquid tags
 parser.format_math()
 parser.format_javascript()
 parser.format_image_links()
+
+#
 parser.nb2md(include_front_matter=True)
+
+"""
+path = '_jupyter/test.ipynb'
+from IPython.display import HTML, Markdown
+md_raw = !jupyter nbconvert --to markdown {path} --stdout # --template=jekyll.tpl
+md_raw = md_raw[1:]
+md = '\n'.join(md_raw)
+display(Markdown(md))
+"""
